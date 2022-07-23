b830672469bef3b312b1b0ed5bd2c8877696a830
gpmatheus <gp.matheusluis@gmail.com>
2022-07-23 18:43:24 -0300

    added threads to handle multiple clients

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import socket
+import socket, threading
 
 HOST = 'localhost'
 PORT = 50000
@@ -7,90 +7,94 @@ memList  = []
 diskList = []
 cpuList = []
 
-
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
-x = 0
 s.listen()
 
-print('Aguardando Conexão de um cliente')
+def handle_client(conn) :
+    x = 0
+    while True :
+        sent_back = False
+        data = conn.recv(1024)
+        entrance = str(data.decode()).split(' ')
+        print('enviou algo!')
 
-conn, ender = s.accept()
+        if entrance[0].lower()=='help':
+            conn.sendall(b'\npost {mem/disk/cpu} {your_name}\nget {mem/disk/cpu} {name}')
 
-print('Conectado em', ender)
+        if entrance[0].lower() == 'post':
 
-while True:
-    data = conn.recv(1024)
-    entrance = str(data.decode()).split(' ')
-    print('enviou algo!')
-
-    if entrance[0].lower()=='help':
-        conn.sendall(b'\npost {mem/disk/cpu} {your_name}\nget {mem/disk/cpu} {name}')
-
-    if entrance[0].lower() == 'post':
-
-        if entrance[1].lower() == 'mem':
-            name = entrance[2]
-            memInfo =''
-            x = 3
-            for x in range(x,entrance.__len__(),x+1):
-                memInfo += entrance[x]
-            memList.append(memInfo)
-            print(memList)
+            if entrance[1].lower() == 'mem':
+                name = entrance[2]
+                memInfo =''
+                x = 3
+                for x in range(x,entrance.__len__(),x+1):
+                    memInfo += entrance[x]
+                memList.append(memInfo)
+                print(memList)
             
-            conn.sendall(b'Guardamos informacoes da memoria ram!')
+                conn.sendall(b'Guardamos informacoes da memoria ram!')
+                sent_back = True
 
-        if entrance[1].lower() == 'disk':
-            name = entrance[2]
-            diskInfo =''
-            x = 3
-            for x in range(x,entrance.__len__(),x+1):
-                diskInfo += entrance[x]
-            diskList.append(diskInfo)
-            print(diskList)
+            if entrance[1].lower() == 'disk':
+                name = entrance[2]
+                diskInfo =''
+                x = 3
+                for x in range(x,entrance.__len__(),x+1):
+                    diskInfo += entrance[x]
+                diskList.append(diskInfo)
+                print(diskList)
             
-            conn.sendall(b'Guardamos informacoes do disco!')
+                conn.sendall(b'Guardamos informacoes do disco!')
+                sent_back = True
 
-        if entrance[1].lower() == 'cpu':
-            name = entrance[2]
-            cpuInfo =''
-            x = 3
-            for x in range(x,entrance.__len__(),x+1):
-                cpuInfo += entrance[x]
-            cpuList.append(cpuInfo)
-            print(cpuList)
+            if entrance[1].lower() == 'cpu':
+                name = entrance[2]
+                cpuInfo =''
+                x = 3
+                for x in range(x,entrance.__len__(),x+1):
+                    cpuInfo += entrance[x]
+                cpuList.append(cpuInfo)
+                print(cpuList)
             
-            conn.sendall(b'Guardamos informacoes da cpu!')
+                conn.sendall(b'Guardamos informacoes da cpu!')
+                sent_back = True
 
-    if entrance[0].lower() == 'get':
+        if entrance[0].lower() == 'get':
 
-        if entrance[1].lower() == 'mem':
-            my_bytes = b''
-            my_str = memList[0]
-            result = my_bytes + my_str.encode('utf-8')
-            print(result)
-            conn.sendall(result)
+            if entrance[1].lower() == 'mem':
+                my_bytes = b''
+                my_str = memList[0]
+                result = my_bytes + my_str.encode('utf-8')
+                print(result)
+                conn.sendall(result)
+                sent_back = True
         
-        if entrance[1].lower() == 'disk':
-            my_bytes = b''
-            my_str = diskList[0]
-            result = my_bytes + my_str.encode('utf-8')
-            print(result)
-            conn.sendall(result)
+            if entrance[1].lower() == 'disk':
+                my_bytes = b''
+                my_str = diskList[0]
+                result = my_bytes + my_str.encode('utf-8')
+                print(result)
+                conn.sendall(result)
+                sent_back = True
 
-        if entrance[1].lower() == 'cpu':
-            my_bytes = b''
-            my_str = cpuList[0]
-            result = my_bytes + my_str.encode('utf-8')
-            print(result)
-            conn.sendall(result)
-
+            if entrance[1].lower() == 'cpu':
+                my_bytes = b''
+                my_str = cpuList[0]
+                result = my_bytes + my_str.encode('utf-8')
+                print(result)
+                conn.sendall(result)
+                sent_back = True
+        
+        if not sent_back :
+            conn.sendall(b'Requisicao mal feita')
     
-    if not data:
-        print('fechando conexão')
-        conn.close()
-        break
-    
+        if not data:
+            print('fechando conexão')
+            conn.close()
+            break
 
-
+while True : 
+    conn, addres = s.accept()
+    thread = threading.Thread(target=handle_client, args=(conn,))
+    thread.start()
